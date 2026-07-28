e6d7bcc83d5c8dcf98e2b9353e67d1cbbec7d619
shreeharan44 <shreeharan44@gmail.com>
2026-07-28 19:10:16 +0530

    fix 4

--- src/components/showcase/DeviceFrame.tsx
+++ src/components/showcase/DeviceFrame.tsx
@@ -19,18 +19,19 @@ export function DeviceFrame({ children, progress }: Props) {
       className="absolute inset-0 flex items-center justify-center"
       style={{ perspective: "2200px" }}
     >
-      <motion.div
-        className="relative max-h-[76vh] max-w-[84vw]"
-        style={{
-          width: "min(980px, 82vw, 102vh)",
-          aspectRatio: "16 / 10",
-          pointerEvents: "auto",
-          rotateX,
-          rotateY,
-          rotateZ,
-          scale,
-        }}
-      >
+      <div className="device-frame-wrapper relative max-h-[76vh] max-w-[84vw]">
+        <motion.div
+          className="relative h-full w-full"
+          style={{
+            width: "min(980px, 82vw, 102vh)",
+            aspectRatio: "16 / 10",
+            pointerEvents: "auto",
+            rotateX,
+            rotateY,
+            rotateZ,
+            scale,
+          }}
+        >
 
 
         {/* device shadow */}
